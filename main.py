9d8ef10c1f91ebc7abeaab0df86465fb5fa7820b
kennethauestad <39300777+kennethauestad@users.noreply.github.com>
2025-07-21 15:26:55 +0000

    Test med Flask

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def hils(navn):
-    print("Hei", navn)
+from flask import Flask, render_template # importerer Flask og render_template fra flask
 
-hils("Ola")
+app = Flask(__name__)  
+@app.route("/")
 
+def hei():
+    return "Hei Per, velkommen til Flask!"
 
+if __name__ == "__main__":
+    app.run()
